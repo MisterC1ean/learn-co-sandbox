6586a8858bf852bb627477be2fc71f37788fbac3
Robert Keller <robert_keller44@icloud.com>
2020-02-04 20:29:21 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting_a_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_a_programmer("Maria" , "Ruby")
+
+greeting_a_programmer("Robert" , "Javascript")
